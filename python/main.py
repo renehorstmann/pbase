925b7dd6a25b3ff2852fb0dbdf46b654aa172039
renehorstmann <r.horsty@gmail.com>
2021-08-03 16:17:20 +0200

    ctypes Structure array bug fix (unrolled arrays...)

--- python/main.py
+++ python/main.py
@@ -1,22 +1,21 @@
 import pbase as p
 
+import ctypes as ct
 import numpy as np
 
-
 if __name__ == '__main__':
+    print('start')
 
-    print('hey')
-    test = p.geoc.quat_eye()
-    print(test)
-    test = p.geoc.quat_conj(test)
-    print(test)
+    rot_a = p.geoc.rotation_from_angle_axis([0, 0, 1, np.radians(45)])
+    rot_b = p.geoc.rotation_from_angle_axis([0, 0, 1, np.radians(135)])
 
-    # rot_a = p.geoc.rotation_from_angle_axis([0, 0, 1, np.radians(45)])
-    # rot_b = p.geoc.rotation_from_angle_axis([0, 0, 1, np.radians(135)])
-    #
-    # quat_a = p.geoc.quat_from_rotation_matrix(rot_a)
-    # quat_b = p.geoc.quat_from_rotation_matrix(rot_b)
-    #
-    # quat_ab = p.geoc.quat_slerp(quat_a, quat_b, 0.5)
-    #
-    # print(p.geoc.quat_to_angle_axis(quat_ab))
+    quat_a = p.geoc.quat_from_rotation_matrix(rot_a)
+    quat_b = p.geoc.quat_from_rotation_matrix(rot_b)
+
+    quat_ab = p.geoc.quat_slerp(quat_a, quat_b, 0.5)
+
+    res_aaxis = p.geoc.quat_to_angle_axis(quat_ab)
+    print(res_aaxis)
+    print(np.degrees(res_aaxis[3]))
+
+    print('fin')
--- python/pbase/mathc.py
+++ python/pbase/mathc.py
@@ -5,64 +5,156 @@ from typing import Optional, Union
 # array like type
 Array = Union[np.ndarray, list, tuple]
 
+c_float_p = ct.POINTER(ct.c_float)
+c_double_p = ct.POINTER(ct.c_double)
+c_int_p = ct.POINTER(ct.c_int)
+
+# warning:
+# a ct.Structure with ('v', ct.c_float * 4) fails to pass to library functions!
+
 class vec2(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * 2)
+        ('v0', ct.c_float),
+        ('v1', ct.c_float)
     ]
 
 class vec3(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * 3)
+        ('v0', ct.c_float),
+        ('v1', ct.c_float),
+        ('v2', ct.c_float)
     ]
 
 class vec4(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * 4)
+        ('v0', ct.c_float),
+        ('v1', ct.c_float),
+        ('v2', ct.c_float),
+        ('v3', ct.c_float)
     ]
 
 class mat2(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * (2*2))
-    ]  
+        ('v0', ct.c_float),
+        ('v1', ct.c_float),
+        ('v2', ct.c_float),
+        ('v3', ct.c_float)
+    ]
     
 class mat3(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * (3*3))
+        ('v0', ct.c_float),
+        ('v1', ct.c_float),
+        ('v2', ct.c_float),
+        ('v3', ct.c_float),
+        ('v4', ct.c_float),
+        ('v5', ct.c_float),
+        ('v6', ct.c_float),
+        ('v7', ct.c_float),
+        ('v8', ct.c_float)
     ]
     
 class mat4(ct.Structure):
     _fields_ = [
-        ('v', ct.c_float * (4*4))
-    ]  
+        ('v0', ct.c_float),
+        ('v1', ct.c_float),
+        ('v2', ct.c_float),
+        ('v3', ct.c_float),
+        ('v4', ct.c_float),
+        ('v5', ct.c_float),
+        ('v6', ct.c_float),
+        ('v7', ct.c_float),
+        ('v8', ct.c_float),
+        ('v9', ct.c_float),
+        ('v10', ct.c_float),
+        ('v11', ct.c_float),
+        ('v12', ct.c_float),
+        ('v13', ct.c_float),
+        ('v14', ct.c_float),
+        ('v15', ct.c_float)
+    ]
 
 class dvec2(ct.Structure):
     _fields_ = [
-        ('v', ct.c_double * 2)
+        ('v0', ct.c_double),
+        ('v1', ct.c_double)
     ]
 
 class dvec3(ct.Structure):
     _fields_ = [
-        ('v', ct.c_double * 3)
+        ('v0', ct.c_double),
+        ('v1', ct.c_double),
+        ('v2', ct.c_double)
     ]
 
 class dvec4(ct.Structure):
     _fields_ = [
-        ('v', ct.c_double * 4)
+        ('v0', ct.c_double),
+        ('v1', ct.c_double),
+        ('v2', ct.c_double),
+        ('v3', ct.c_double)
+    ]
+
+class dmat2(ct.Structure):
+    _fields_ = [
+        ('v0', ct.c_double),
+        ('v1', ct.c_double),
+        ('v2', ct.c_double),
+        ('v3', ct.c_double)
+    ]
+
+class dmat3(ct.Structure):
+    _fields_ = [
+        ('v0', ct.c_double),
+        ('v1', ct.c_double),
+        ('v2', ct.c_double),
+        ('v3', ct.c_double),
+        ('v4', ct.c_double),
+        ('v5', ct.c_double),
+        ('v6', ct.c_double),
+        ('v7', ct.c_double),
+        ('v8', ct.c_double)
+    ]
+
+class dmat4(ct.Structure):
+    _fields_ = [
+        ('v0', ct.c_double),
+        ('v1', ct.c_double),
+        ('v2', ct.c_double),
+        ('v3', ct.c_double),
+        ('v4', ct.c_double),
+        ('v5', ct.c_double),
+        ('v6', ct.c_double),
+        ('v7', ct.c_double),
+        ('v8', ct.c_double),
+        ('v9', ct.c_double),
+        ('v10', ct.c_double),
+        ('v11', ct.c_double),
+        ('v12', ct.c_double),
+        ('v13', ct.c_double),
+        ('v14', ct.c_double),
+        ('v15', ct.c_double)
     ]
 
 class ivec2(ct.Structure):
     _fields_ = [
-        ('v', ct.c_int * 2)
+        ('v0', ct.c_int),
+        ('v1', ct.c_int)
     ]
 
 class ivec3(ct.Structure):
     _fields_ = [
-        ('v', ct.c_int * 3)
+        ('v0', ct.c_int),
+        ('v1', ct.c_int),
+        ('v2', ct.c_int)
     ]
 
 class ivec4(ct.Structure):
     _fields_ = [
-        ('v', ct.c_int * 4)
+        ('v0', ct.c_int),
+        ('v1', ct.c_int),
+        ('v2', ct.c_int),
+        ('v3', ct.c_int)
     ]
 
 vec2_p = ct.POINTER(vec2)
@@ -74,6 +166,9 @@ mat4_p = ct.POINTER(mat4)
 dvec2_p = ct.POINTER(dvec2)
 dvec3_p = ct.POINTER(dvec3)
 dvec4_p = ct.POINTER(dvec4)
+dmat2_p = ct.POINTER(dmat2)
+dmat3_p = ct.POINTER(dmat3)
+dmat4_p = ct.POINTER(dmat4)
 ivec2_p = ct.POINTER(ivec2)
 ivec3_p = ct.POINTER(ivec3)
 ivec4_p = ct.POINTER(ivec4)
@@ -83,7 +178,7 @@ ivec4_p = ct.POINTER(ivec4)
 #
 
 def cast_vec2_np(data: vec2) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(2,))
 
 def cast_np_vec2_p(data: np.ndarray) -> vec2_p:
     if data is None:
@@ -103,7 +198,7 @@ def cast_array_vec2(data) -> vec2:
 
 
 def cast_vec3_np(data: vec3) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(3,))
 
 def cast_np_vec3_p(data: np.ndarray) -> vec3_p:
     if data is None:
@@ -123,7 +218,7 @@ def cast_array_vec3(data) -> vec3:
 
 
 def cast_vec4_np(data: vec4) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(4,))
 
 def cast_np_vec4_p(data: np.ndarray) -> vec4_p:
     if data is None:
@@ -144,7 +239,7 @@ def cast_array_vec4(data) -> vec4:
 
 
 def cast_mat2_np(data: mat2) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v).reshape(2, 2)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(2, 2))
 
 def cast_np_mat2_p(data: np.ndarray) -> mat2_p:
     if data is None:
@@ -168,7 +263,7 @@ def cast_array_mat2(data) -> mat2:
 
 
 def cast_mat3_np(data: mat3) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v).reshape(3, 3)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(3, 3))
 
 def cast_np_mat3_p(data: np.ndarray) -> mat3_p:
     if data is None:
@@ -176,7 +271,7 @@ def cast_np_mat3_p(data: np.ndarray) -> mat3_p:
     else:
         if data.dtype != np.float32:
             raise RuntimeError('cast_np_mat3_p failed, wrong type')
-        if data.ndim != 3 or data.shape[0] != 3 or data.shape[1] != 3:
+        if data.ndim != 2 or data.shape[0] != 3 or data.shape[1] != 3:
             raise RuntimeError('cast_np_mat3_p failed, wrong size')
         if np.isfortran(data):
             raise RuntimeError('cast_np_mat3_p failed: must be C order')
@@ -192,7 +287,7 @@ def cast_array_mat3(data) -> mat3:
 
 
 def cast_mat4_np(data: mat4) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v).reshape(4, 4)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_float_p), shape=(4, 4))
 
 def cast_np_mat4_p(data: np.ndarray) -> mat4_p:
     if data is None:
@@ -200,7 +295,7 @@ def cast_np_mat4_p(data: np.ndarray) -> mat4_p:
     else:
         if data.dtype != np.float32:
             raise RuntimeError('cast_np_mat4_p failed, wrong type')
-        if data.ndim != 4 or data.shape[0] != 4 or data.shape[1] != 4:
+        if data.ndim != 2 or data.shape[0] != 4 or data.shape[1] != 4:
             raise RuntimeError('cast_np_mat4_p failed, wrong size')
         if np.isfortran(data):
             raise RuntimeError('cast_np_mat4_p failed: must be C order')
@@ -218,7 +313,7 @@ def cast_array_mat4(data) -> mat4:
 
 
 def cast_dvec2_np(data: dvec2) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(2,))
 
 def cast_np_dvec2_p(data: np.ndarray) -> dvec2_p:
     if data is None:
@@ -238,7 +333,7 @@ def cast_array_dvec2(data) -> dvec2:
 
 
 def cast_dvec3_np(data: dvec3) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(3,))
 
 def cast_np_dvec3_p(data: np.ndarray) -> dvec3_p:
     if data is None:
@@ -258,7 +353,7 @@ def cast_array_dvec3(data) -> dvec3:
 
 
 def cast_dvec4_np(data: dvec4) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(4,))
 
 def cast_np_dvec4_p(data: np.ndarray) -> dvec4_p:
     if data is None:
@@ -277,9 +372,81 @@ def cast_array_dvec4(data) -> dvec4:
     return cast_np_dvec4(np.float64(data))
 
 
+def cast_dmat2_np(data: dmat2) -> np.ndarray:
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(2, 2))
+
+def cast_np_dmat2_p(data: np.ndarray) -> dmat2_p:
+    if data is None:
+        data = np.zeros(4, dtype=np.float64)
+    else:
+        if data.dtype != np.float64:
+            raise RuntimeError('cast_np_dmat2_p failed, wrong type')
+        if data.ndim != 2 or data.shape[0] != 2 or data.shape[1] != 2:
+            raise RuntimeError('cast_np_dmat2_p failed, wrong size')
+        if np.isfortran(data):
+            raise RuntimeError('cast_np_dmat2_p failed: must be C order')
+    return data.ctypes.data_as(dmat2_p)
+
+def cast_np_dmat2(data: np.ndarray) -> dmat2:
+    return cast_np_dmat2_p(data).contents
+
+def cast_array_dmat2(data) -> dmat2:
+    if data is None:
+        data = np.zeros(4, dtype=np.float64)
+    return cast_np_dmat2(np.float64(data))
+
+
+def cast_dmat3_np(data: dmat3) -> np.ndarray:
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(3, 3))
+
+def cast_np_dmat3_p(data: np.ndarray) -> dmat3_p:
+    if data is None:
+        data = np.zeros(3*3, dtype=np.float64)
+    else:
+        if data.dtype != np.float64:
+            raise RuntimeError('cast_np_dmat3_p failed, wrong type')
+        if data.ndim != 2 or data.shape[0] != 3 or data.shape[1] != 3:
+            raise RuntimeError('cast_np_dmat3_p failed, wrong size')
+        if np.isfortran(data):
+            raise RuntimeError('cast_np_dmat3_p failed: must be C order')
+    return data.ctypes.data_as(dmat3_p)
+
+def cast_np_dmat3(data: np.ndarray) -> dmat3:
+    return cast_np_dmat3_p(data).contents
+
+def cast_array_dmat3(data) -> dmat3:
+    if data is None:
+        data = np.zeros(3*3, dtype=np.float64)
+    return cast_np_dmat3(np.float64(data))
+
+
+def cast_dmat4_np(data: dmat4) -> np.ndarray:
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_double_p), shape=(4, 4))
+
+def cast_np_dmat4_p(data: np.ndarray) -> dmat4_p:
+    if data is None:
+        data = np.zeros(4*4, dtype=np.float64)
+    else:
+        if data.dtype != np.float64:
+            raise RuntimeError('cast_np_dmat4_p failed, wrong type')
+        if data.ndim != 2 or data.shape[0] != 4 or data.shape[1] != 4:
+            raise RuntimeError('cast_np_dmat4_p failed, wrong size')
+        if np.isfortran(data):
+            raise RuntimeError('cast_np_dmat4_p failed: must be C order')
+    return data.ctypes.data_as(dmat4_p)
+
+def cast_np_dmat4(data: np.ndarray) -> dmat4:
+    return cast_np_dmat4_p(data).contents
+
+def cast_array_dmat4(data) -> dmat4:
+    if data is None:
+        data = np.zeros(4*4, dtype=np.float64)
+    return cast_np_dmat4(np.float64(data))
+
+
 
 def cast_ivec2_np(data: ivec2) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_int_p), shape=(2,))
 
 def cast_np_ivec2_p(data: np.ndarray) -> ivec2_p:
     if data is None:
@@ -299,7 +466,7 @@ def cast_array_ivec2(data) -> ivec2:
 
 
 def cast_ivec3_np(data: ivec3) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_int_p), shape=(3,))
 
 def cast_np_ivec3_p(data: np.ndarray) -> ivec3_p:
     if data is None:
@@ -319,7 +486,7 @@ def cast_array_ivec3(data) -> ivec3:
 
 
 def cast_ivec4_np(data: ivec4) -> np.ndarray:
-    return np.ctypeslib.as_array(data.v)
+    return np.ctypeslib.as_array(ct.cast(ct.pointer(data), c_int_p), shape=(4,))
 
 def cast_np_ivec4_p(data: np.ndarray) -> ivec4_p:
     if data is None:
