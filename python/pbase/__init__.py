47758b262b3591bb21476ad2090204964bb90348
renehorstmann <r.horsty@gmail.com>
2021-08-27 11:47:10 +0200

    windows configuration

--- python/pbase/__init__.py
+++ python/pbase/__init__.py
@@ -1,6 +1,13 @@
+import os
 import ctypes
 
-plib = ctypes.cdll.LoadLibrary('/usr/local/lib/libpbase.so')
+if os.name == 'posix':
+    plib = ctypes.cdll.LoadLibrary('/usr/local/lib/libpbase.so')
+elif os.name == 'nt':
+    plib = ctypes.cdll.LoadLibrary('C:/Libraries/lib/libpbase.dll')
+else:
+    print('unknown os?:', os.name)
+    exit(1)
 
 # after loading lib!
 from . import mathc
--- tests/main.c
+++ tests/main.c
@@ -7,6 +7,9 @@ int main() {
     puts("start");
 
     pCloud points = p_cloud_new_zeros_1(12);
+    for(int i=0; i<points.size; i++) {
+        points.data[i].x = i;
+    }
 
     p_cloud_kill(&points);
 
