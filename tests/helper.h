40c449d40c9e8d510a88a542d52f8fce841224ca
renehorstmann <r.horsty@gmail.com>
2021-09-08 16:31:08 +0200

    ply test

--- tests/helper.h
+++ tests/helper.h
@@ -1,8 +1,10 @@
 #ifndef PBASE_HELPER_H
 #define PBASE_HELPER_H
 
+#include "pbase/mathc/int.h"
 #include "pbase/mathc/float.h"
 #include "pbase/types/cloud.h"
+#include "pbase/types/meshindices.h"
 
 bool equals_vec4(vec4 a, vec4 b) {
     return vec4_norm_1(vec4_sub_vec(a, b)) < 0.01;
@@ -16,6 +18,12 @@ bool equals_cloud(pCloud a, pCloud b) {
             return false;
     }
     return true;
+}
+
+bool equals_mesh_indices(pMeshIndices a, pMeshIndices b) {
+    if(!p_mesh_indices_valid(a) || !p_mesh_indices_valid(b) || a.size != b.size)
+        return false;
+    return memcmp(a.data, b.data, sizeof(*a.data) *a.size) == 0;
 }
 
 #endif //PBASE_HELPER_H
--- tests/test_io.c
+++ tests/test_io.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pbase/io/stl.h"
+#include "pbase/io/io.h"
 #include "helper.h"
 
 
 static void stl() {
     vec4 data_cloud[4] = {
-            {{0, 0, 0, 1}},
-            {{10, 0, 0, 1}},
-            {{0, 10, 0, 1}},
+            {{0,  0,  0, 1}},
+            {{10, 0,  0, 1}},
+            {{0,  10, 0, 1}},
             {{10, 10, 0, 1}}
     };
     ivec3 data_indices[2] = {
@@ -16,11 +16,11 @@ static void stl() {
             {{2, 1, 3}}
     };
     vec4 data_cloud_unpacked[6] = {
-            {{0, 0, 0, 1}},
-            {{10, 0, 0, 1}},
-            {{0, 10, 0, 1}},
-            {{0, 10, 0, 1}},
-            {{10, 0, 0, 1}},
+            {{0,  0,  0, 1}},
+            {{10, 0,  0, 1}},
+            {{0,  10, 0, 1}},
+            {{0,  10, 0, 1}},
+            {{10, 0,  0, 1}},
             {{10, 10, 0, 1}}
     };
 
@@ -29,13 +29,13 @@ static void stl() {
     pMeshIndices save_indices = {data_indices, 2};
 
     p_io_save_mesh_stl(save_points, save_indices, "test_io_ascii.stl", true);
-    if(p_error()) {
+    if (p_error()) {
         printf("p_io_save_mesh_stl ascii failed, %s", p_error());
         exit(1);
     }
 
     p_io_save_mesh_stl(save_points, save_indices, "test_io_binary.stl", false);
-    if(p_error()) {
+    if (p_error()) {
         printf("p_io_save_mesh_stl binary failed, %s", p_error());
         exit(1);
     }
@@ -44,11 +44,11 @@ static void stl() {
     pCloud load_points;
     pMeshIndices load_indices;
     p_io_load_mesh_stl(&load_points, &load_indices, "test_io_ascii.stl");
-    if(p_error()) {
+    if (p_error()) {
         printf("p_io_load_mesh_stl ascii failed, %s", p_error());
         exit(1);
     }
-    if(!equals_cloud(load_points, (pCloud) {data_cloud_unpacked, 6})) {
+    if (!equals_cloud(load_points, (pCloud) {data_cloud_unpacked, 6})) {
         printf("p_io_load_mesh_stl ascii failed, loaded cloud is wrong");
         exit(1);
     }
@@ -56,11 +56,11 @@ static void stl() {
     p_mesh_indices_kill(&load_indices);
 
     p_io_load_mesh_stl(&load_points, &load_indices, "test_io_binary.stl");
-    if(p_error()) {
+    if (p_error()) {
         printf("p_io_load_mesh_stl binary failed, %s", p_error());
         exit(1);
     }
-    if(!equals_cloud(load_points, (pCloud) {data_cloud_unpacked, 6})) {
+    if (!equals_cloud(load_points, (pCloud) {data_cloud_unpacked, 6})) {
         printf("p_io_load_mesh_stl binary failed, loaded cloud is wrong");
         exit(1);
     }
@@ -68,7 +68,93 @@ static void stl() {
     p_mesh_indices_kill(&load_indices);
 }
 
+
+static void ply() {
+    vec4 data_cloud[4] = {
+            {{0,  0,  0, 1}},
+            {{10, 0,  0, 1}},
+            {{0,  10, 0, 1}},
+            {{10, 10, 0, 1}}
+    };
+    vec4 data_normals[4] = {
+            {{-0.5, -0.5, 0.7071, 0}},
+            {{0.5,  -0.5, 0.7071, 0}},
+            {{-0.5, 0.5,  0.7071, 0}},
+            {{0.5,  0.5,  0.7071, 0}}
+    };
+    vec4 data_colors[4] = {
+            {{1, 0, 0, 1}},
+            {{0, 1, 0, 1}},
+            {{0, 0, 1, 1}},
+            {{1, 1, 1, 1}}
+    };
+    ivec3 data_indices[2] = {
+            {{0, 1, 2}},
+            {{2, 1, 3}}
+    };
+
+
+    pCloud save_points = {data_cloud, 4};
+    pCloud save_normals = {data_normals, 4};
+    pCloud save_colors = {data_colors, 4};
+    pMeshIndices save_indices = {data_indices, 2};
+
+    p_io_save_mesh_normals_colored_ply(save_points, save_normals, save_colors, save_indices,
+                                       "test_io_ascii.ply", true);
+    if (p_error()) {
+        printf("p_io_save_mesh_normals_colored_ply ascii failed, %s", p_error());
+        exit(1);
+    }
+
+    p_io_save_mesh_normals_colored_ply(save_points, save_normals, save_colors, save_indices,
+                                       "test_io_binary.ply", true);
+    if (p_error()) {
+        printf("p_io_save_mesh_normals_colored_ply binary failed, %s", p_error());
+        exit(1);
+    }
+
+
+    pCloud load_points;
+    pCloud load_normals;
+    pCloud load_colors;
+    pMeshIndices load_indices;
+    p_io_load_mesh_normals_colored_ply(&load_points, &load_normals, &load_colors, &load_indices, "test_io_ascii.ply");
+    if (p_error()) {
+        printf("p_io_load_mesh_normals_colored_ply ascii failed, %s", p_error());
+        exit(1);
+    }
+    if (!equals_cloud(load_points, save_points)
+        || !equals_cloud(load_normals, save_normals)
+        || !equals_cloud(load_colors, save_colors)
+        || !equals_mesh_indices(load_indices, save_indices)) {
+        printf("p_io_load_mesh_normals_colored_ply ascii failed, loaded data is wrong");
+        exit(1);
+    }
+    p_cloud_kill(&load_points);
+    p_cloud_kill(&load_normals);
+    p_cloud_kill(&load_colors);
+    p_mesh_indices_kill(&load_indices);
+
+    p_io_load_mesh_normals_colored_ply(&load_points, &load_normals, &load_colors, &load_indices, "test_io_binary.ply");
+    if (p_error()) {
+        printf("p_io_load_mesh_normals_colored_ply binary failed, %s", p_error());
+        exit(1);
+    }
+    if (!equals_cloud(load_points, save_points)
+        || !equals_cloud(load_normals, save_normals)
+        || !equals_cloud(load_colors, save_colors)
+        || !equals_mesh_indices(load_indices, save_indices)) {
+        printf("p_io_load_mesh_normals_colored_ply binary failed, loaded data is wrong");
+        exit(1);
+    }
+    p_cloud_kill(&load_points);
+    p_cloud_kill(&load_normals);
+    p_cloud_kill(&load_colors);
+    p_mesh_indices_kill(&load_indices);
+}
+
 int main() {
     stl();
+    ply();
 
 }
