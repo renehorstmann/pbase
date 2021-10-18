810f542291200e0b2a475274cc7bb897e6115de8
renehorstmann <r.horsty@gmail.com>
2021-10-18 13:09:14 +0200

    rhc update

--- include/pbase/rhc/file.h
+++ include/pbase/rhc/file.h
@@ -5,7 +5,6 @@
 #include "alloc.h"
 
 
-
 // reads in a full file into the returned String
 String p_rhc_file_read_a(const char *file, bool ascii, Allocator_s a);
 // writes a file and returns false on error
@@ -14,28 +13,23 @@ bool p_rhc_file_write(const char *file, Str_s content, bool ascii);
 bool p_rhc_file_append(const char *file, Str_s content, bool ascii);
 
 
-
 // reads in a full file into the returned String
 static String file_read(const char *file, bool ascii) {
     return p_rhc_file_read_a(file, ascii, RHC_STRING_DEFAULT_ALLOCATOR);
 }
 
+//
 // wrapper without _rhc
+//
 
 // reads in a full file into the returned String
-static String file_read_a(const char *file, bool ascii, Allocator_s a) {
-    return p_rhc_file_read_a(file, ascii, a);
-}
+#define file_read_a p_rhc_file_read_a
 
 // writes a file and returns false on error
-static bool file_write(const char *file, Str_s content, bool ascii) {
-    return p_rhc_file_write(file, content, ascii);
-}
+#define file_write p_rhc_file_write
 
 // appends to a file and returns false on error
-static bool file_append(const char *file, Str_s content, bool ascii) {
-    return p_rhc_file_append(file, content, ascii);
-}
+#define file_append p_rhc_file_append
 
 
 #endif //RHC_FILE_H
---
+++ include/pbase/rhc/impl/socket_impl.h
@@ -0,0 +1,259 @@
+#ifndef RHC_SOCKET_IMPL_H
+#define RHC_SOCKET_IMPL_H
+#ifdef OPTION_SOCKET
+
+#include <assert.h>
+#include "../socket.h"
+#include "../alloc.h"
+
+#ifdef WIN32
+#include <winsock2.h>
+#include <winsock.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+
+typedef struct {
+    SOCKET so;
+} WinSocket;
+_Static_assert(sizeof (WinSocket) <= RHC_SOCKET_STORAGE_SIZE, "storage not big enough");
+
+
+bool p_rhc_socketserver_valid(SocketServer self) {
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+    return impl->so != INVALID_SOCKET;
+}
+
+SocketServer p_rhc_socketserver_new_invalid() {
+    SocketServer self = {0};
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+    impl->so = INVALID_SOCKET;
+    return self;
+}
+
+SocketServer p_rhc_socketserver_new(const char *address, const char *port) {
+    SocketServer self = {0};
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+
+    // winsock startup (can be called multiple times...)
+    {
+        WORD version = MAKEWORD(2, 2);
+        WSADATA wsadata;
+        int status = WSAStartup(version, &wsadata);
+        if(status != 0) {
+            log_error("p_rhc_socketserver_new failed, WSAStartup failed: &i", status);
+            p_rhc_error = "p_rhc_socketserver_new failed";
+            return p_rhc_socketserver_new_invalid();
+        }
+    }
+
+    struct addrinfo hints = {
+            .ai_family = AF_UNSPEC,
+            .ai_socktype = SOCK_STREAM
+    };
+//    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    // find a valid address and create a socket on it
+    {
+        struct addrinfo *servinfo;
+        int status = getaddrinfo(address, port, &hints, &servinfo);
+        if (status != 0) {
+            log_error("p_rhc_socketserver_new failed: getaddrinfo error: %s\n", gai_strerror(status));
+            p_rhc_error = "p_rhc_socketserver_new failed";
+            return p_rhc_socketserver_new_invalid();
+        }
+
+        for (struct addrinfo *ai = servinfo; ai != NULL; ai = ai->ai_next) {
+            impl->so = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+            if (!p_rhc_socketserver_valid(self))
+                continue;
+
+            if(bind(impl->so, ai->ai_addr, (int) ai->ai_addrlen) == -1) {
+                p_rhc_socketserver_kill(&self);
+                continue;
+            }
+
+            // valid socket + bind
+            break;
+        }
+
+        freeaddrinfo(servinfo); // free the linked-list
+    }
+
+    // no valid address found?
+    if(!p_rhc_socketserver_valid(self)) {
+        log_error("p_rhc_socketserver_new failed to create the server socket");
+        p_rhc_error = "p_rhc_socketserver_new failed";
+        return p_rhc_socketserver_new_invalid();
+    }
+
+    // reuse socket port
+    {
+        BOOL yes = 1;
+        setsockopt(impl->so, SOL_SOCKET, SO_REUSEADDR, (char*) &yes, sizeof yes);
+    }
+
+    int backlog = 10;   // queue size of incoming connections
+    if(listen(impl->so, backlog) == -1) {
+        log_error("p_rhc_socketserver_new failed to listen");
+        p_rhc_error = "p_rhc_socketserver_new failed";
+        p_rhc_socketserver_kill(&self);
+    }
+
+    return self;
+}
+
+void p_rhc_socketserver_kill(SocketServer *self) {
+    WinSocket *impl = (WinSocket *) self->impl_storage;
+    closesocket(impl->so);
+    *self = p_rhc_socketserver_new_invalid();
+}
+
+Socket p_rhc_socketserver_accept(SocketServer *self) {
+    if(!p_rhc_socketserver_valid(*self))
+        return p_rhc_socket_new_invalid();
+
+    WinSocket *impl = (WinSocket *) self->impl_storage;
+
+    Socket client = {0};
+    WinSocket *client_impl = (WinSocket *) client.impl_storage;
+
+    struct sockaddr_storage addr;
+    int addrlen = sizeof addr;
+    client_impl->so = accept(impl->so, (struct sockaddr *) &addr, &addrlen);
+
+    if(!p_rhc_socket_valid(client)) {
+        log_error("p_rhc_socketserver_accept failed, killing the server");
+        p_rhc_socketserver_kill(self);
+        return p_rhc_socket_new_invalid();
+    }
+
+    char *client_ip = inet_ntoa(((struct sockaddr_in *) &addr)->sin_addr);
+    log_info("p_rhc_socketserver_accept connected with: %s", client_ip);
+
+    return client;
+}
+
+bool p_rhc_socket_valid(Socket self) {
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+    return impl->so != INVALID_SOCKET;
+}
+
+Socket p_rhc_socket_new_invalid() {
+    Socket self = {0};
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+    impl->so = INVALID_SOCKET;
+    return self;
+}
+
+Socket p_rhc_socket_new(const char *address, const char *port) {
+    Socket self = {0};
+    WinSocket *impl = (WinSocket *) self.impl_storage;
+
+    // winsock startup (can be called multiple times...)
+    {
+        WORD version = MAKEWORD(2, 2);
+        WSADATA wsadata;
+        int status = WSAStartup(version, &wsadata);
+        if(status != 0) {
+            log_error("p_rhc_socket_new_server failed, WSAStartup failed: &i", status);
+            p_rhc_error = "p_rhc_socket_new_server failed";
+            return p_rhc_socket_new_invalid();
+        }
+    }
+
+    struct addrinfo hints = {
+            .ai_family = AF_UNSPEC,
+            .ai_socktype = SOCK_STREAM
+    };
+
+    // find a valid address and connect to it
+    {
+        struct addrinfo *servinfo;
+        int status = getaddrinfo(address, port, &hints, &servinfo);
+        if (status != 0) {
+            log_error("p_rhc_socket_new failed: getaddrinfo error: %s\n", gai_strerror(status));
+            p_rhc_error = "p_rhc_socket_new failed";
+            return p_rhc_socket_new_invalid();
+        }
+
+        for (struct addrinfo *ai = servinfo; ai != NULL; ai = ai->ai_next) {
+            impl->so = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+            if (!p_rhc_socket_valid(self))
+                continue;
+
+            if(connect(impl->so, ai->ai_addr, (int) ai->ai_addrlen) == -1) {
+                p_rhc_socket_kill(&self);
+                continue;
+            }
+
+            // valid socket + connect
+            break;
+        }
+
+        freeaddrinfo(servinfo); // free the linked-list
+    }
+
+    // no valid address found?
+    if(!p_rhc_socket_valid(self)) {
+        log_error("p_rhc_socket_new failed to create the connection");
+        p_rhc_error = "p_rhc_socket_new failed";
+        return p_rhc_socket_new_invalid();
+    }
+
+    return self;
+}
+
+void p_rhc_socket_kill(Socket *self) {
+    WinSocket *impl = (WinSocket *) self->impl_storage;
+    closesocket(impl->so);
+    *self = p_rhc_socket_new_invalid();
+}
+
+bool p_rhc_socket_recv(Socket *self, void *msg, size_t size) {
+    if(!p_rhc_socket_valid(*self))
+        false;
+
+    WinSocket *impl = (WinSocket *) self->impl_storage;
+
+    char *buf = msg;
+    while(size > 0) {
+        int n = recv(impl->so, buf, (int) size, 0);
+        if(n <= 0) {
+            log_error("p_rhc_socket_recv failed, killing socket...");
+            p_rhc_socket_kill(self);
+            return false;
+        }
+        assert(n <= size);
+        buf += n;
+        size -= n;
+    }
+    return true;
+}
+
+bool p_rhc_socket_send(Socket *self, const void *msg, size_t size) {
+    if(!p_rhc_socket_valid(*self))
+        false;
+
+    WinSocket *impl = (WinSocket *) self->impl_storage;
+
+    const char *buf = msg;
+    while(size > 0) {
+        int n = send(impl->so, buf, (int) size, 0);
+        if(n <= 0) {
+            log_error("p_rhc_socket_send failed, killing socket...");
+            p_rhc_socket_kill(self);
+            return false;
+        }
+        assert(n <= size);
+
+        buf += n;
+        size -= n;
+    }
+    return true;
+}
+
+#endif
+
+
+#endif //OPTION_SOCKET
+#endif //RHC_SOCKET_IMPL_H
